118a05e449ad06335920ee259178ee995c741234
Misael Guerrero <mguerrerowest@gmail.com>
2023-02-18 22:14:44 -0500

    Submit button on form creates new book objects and logs them on the console

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const newBookBtn = document.querySelector('#newBookBtn')
+const submitBook = document.querySelector('#submitBook')
+
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -13,9 +15,16 @@ function addBookToLibrary() {
     let author = document.querySelector('#author').value;
     let pages = document.querySelector('#pages').value;
     let read = document.querySelector('#read').value;
+    let newBook = new Book(title, author, pages, read)
+    console.log(newBook)
 }
 
 newBookBtn.addEventListener('click', function(){
     const newBookForm = document.querySelector('#newBookForm')
     newBookForm.style.display = 'block'
+})
+
+submitBook.addEventListener('click', function(event){
+    event.preventDefault()
+    addBookToLibrary()
 })
