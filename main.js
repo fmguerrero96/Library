ea9c31eb123de8e9b538c43aa523b5b86c44451a
Misael Guerrero <mguerrerowest@gmail.com>
2023-02-18 21:31:54 -0500

    Add button to bring up the 'new book' form

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+const newBookBtn = document.querySelector('#newBookBtn')
+let myLibrary = [];
 
+function Book() {
+// the constructor
+}
+
+function addBookToLibrary() {
+
+}
+
+newBookBtn.addEventListener('click', function(){
+    const newBookForm = document.querySelector('#newBookForm')
+    newBookForm.style.display = 'block'
+})
