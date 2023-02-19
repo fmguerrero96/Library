59423d0a2eae28cb97d9acd28cea52401afd8a14
Misael Guerrero <mguerrerowest@gmail.com>
2023-02-18 21:54:12 -0500

    Create book constructor and function to add books to library

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 const newBookBtn = document.querySelector('#newBookBtn')
 let myLibrary = [];
 
-function Book() {
-// the constructor
+function Book(title, author, pages, read) {
+    this.title = title,
+    this.author = author,
+    this.pages = pages,
+    this.read = read
 }
 
 function addBookToLibrary() {
-
+    let title = document.querySelector('#title').value;
+    let author = document.querySelector('#author').value;
+    let pages = document.querySelector('#pages').value;
+    let read = document.querySelector('#read').value;
 }
 
 newBookBtn.addEventListener('click', function(){
